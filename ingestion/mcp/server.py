15126147b9890b6814ff5a0e1121e027fd90e252
Joywalker <alexandrurazvant@gmail.com>
2025-05-05 18:27:10 +0300

    fix: Isolate tools from server on MCP creation

--- ingestion/mcp/server.py
+++ ingestion/mcp/server.py
@@ -1,4 +1,32 @@
-from tools import mcp
+from fastmcp import FastMCP
+from tools import add_video, get_clips, list_tables
+
+mcp = FastMCP("VideoProcessor")
+
+mcp.add_tool(
+    name="add_video",
+    fn=add_video,
+    description="Add a new video to database.",
+    tags=["video", "ingestion"],
+)
+
+
+mcp.add_tool(
+    name="fetch_clip",
+    fn=get_clips,
+    description="Fetch a video clip based on a user query.",
+    tags=["video", "query"],
+)
+
+mcp.add_tool(
+    name="list_videos",
+    fn=list_tables,
+    description="List all processed videos in database.",
+    tags=["video", "list"],
+)
 
 if __name__ == "__main__":
-    mcp.run(transport="sse", port=9090, host="127.0.0.1")
+    # FIXME: this is dirty, pass the config
+    # or, use uv run fastmcp run ... and cli pass the args
+    cfg = {"host": "127.0.0.1", "port": 8000, "transport": "sse"}
+    mcp.run(**cfg)
