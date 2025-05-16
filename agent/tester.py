432e6f9cb1565a7e742510a20f023699727cbf91
Joywalker <alexandrurazvant@gmail.com>
2025-05-16 17:55:25 +0300

    chore: Update notebook

--- agent/tester.py
+++ agent/tester.py
@@ -1,5 +1,6 @@
 import asyncio
 
+import pixeltable as pxt
 from fastmcp import Client
 
 
@@ -7,7 +8,7 @@ async def test_mcp():
     mcp_server = Client("http://127.0.0.1:8000/sse")
     async with mcp_server:
         tools = await mcp_server.list_tools()
-        # pxt.func.tools.Tool
+        pxt.func.tools.Tool()
         print(f"Discovered tools: {tools}")
 
 
