f988094cb673de2b7c38a9fc66d777169475370a
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2025-05-30 23:10:40 +0200

    feat: new mcp-server python project

---
+++ mcp-server/server.py
@@ -0,0 +1,38 @@
+from fastmcp import FastMCP
+
+from tools import (
+    get_clip_by_caption_sim,
+    get_clip_by_image_sim,
+    get_clip_by_speech_sim,
+    list_tables,
+)
+
+mcp = FastMCP("VideoProcessor")
+
+mcp.add_tool(
+    name="list_tables",
+    description="List all processed videos in the database.",
+    fn=list_tables,
+    tags={"video", "list"},
+)
+
+mcp.add_tool(
+    name="get_clip_by_speech_sim",
+    description="Get a video clip based on a user query using the transcripts index.",
+    fn=get_clip_by_speech_sim,
+    tags={"video", "search", "transcript"},
+)
+
+mcp.add_tool(
+    name="get_clip_by_image_sim",
+    description="Get a video clip based on a user query using the image index.",
+    fn=get_clip_by_image_sim,
+    tags={"video", "search", "image"},
+)
+
+mcp.add_tool(
+    name="get_clip_by_caption_sim",
+    description="Get a video clip based on a user query using the caption index.",
+    fn=get_clip_by_caption_sim,
+    tags={"video", "search", "caption"},
+)
