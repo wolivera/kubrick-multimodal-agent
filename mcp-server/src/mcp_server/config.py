bbbdcee7f0e51ab527589e4c70404895b662f9a4
Joywalker <alexandrurazvant@gmail.com>
2025-06-04 18:33:23 +0300

    fix(env): Add OPIK to env config

--- mcp-server/src/mcp_server/config.py
+++ mcp-server/src/mcp_server/config.py
@@ -4,7 +4,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file="mcp-server/.env", extra="ignore", env_file_encoding="utf-8"
+    )
+
+    # --- OPIK Configuration ---
+    OPIK_API_KEY: str
+    OPIK_WORKSPACE: str = "default"
+    OPIK_PROJECT: str = "mcp-server"
+
     # --- GROQ Configuration ---
     GROQ_API_KEY: str
     GROQ_VLM_MODEL: str = "meta-llama/llama-4-scout-17b-16e-instruct"
